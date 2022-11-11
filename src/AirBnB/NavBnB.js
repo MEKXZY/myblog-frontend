bbc113275d41c359f08c372b25b49a329d40fddc
3m3qa <chukwue3@gmail.com>
2022-11-11 15:40:35 +0100

    initial commit

---
+++ src/AirBnB/NavBnB.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavBnB() {
+  return (
+    <div>
+        <nav>
+            <img  src ='./airbnb 1.png' alt=''/>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBnB
---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBnB from './AirBnB/NavBnB'
+
+function App() {
+  return (
+    <div>
+      <NavBnB/>
+    </div>
+  );
+}
+
+export default App;
